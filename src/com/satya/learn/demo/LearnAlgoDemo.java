08d02eab7f4a1400f305b04f60cfe1fc7a506bba
Satyendra <satyendra070091@gmail.com>
2017-08-23 00:37:08 +0530

    Algorithm Demo

---
+++ src/com/satya/learn/demo/LearnAlgoDemo.java
@@ -0,0 +1,22 @@
+package com.satya.learn.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.satya.learn.java.algos.sorting.SelectionSort;
+import com.satya.learn.java.algos.sorting.Sort;
+
+public class LearnAlgoDemo {
+
+	public static void main(String[] args) {
+		Integer[] elements = new Integer[] { 8, 4, 5, 3, 1, 6, 2 };
+		List<Integer> list = new ArrayList<>(Arrays.asList(elements));
+		
+		
+		Sort<Integer> sorting = new SelectionSort<>();
+		sorting.sort(list);
+		list.forEach(System.out::println);
+	}
+
+}
